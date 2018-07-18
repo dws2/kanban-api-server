09d18ada7e0a374ba358f22ee008f7fa15d3e085
markmichon <hello@markmichon.com>
2018-07-18 17:49:05 -0400

    Fix missing dep

--- api/list/list.router.js
+++ api/list/list.router.js
@@ -1,15 +1,10 @@
 import express from 'express'
 import * as actions from './list.actions'
-import { body } from 'express-validator/check'
 
 
 
 export const listRouter = express.Router()
 
-const checks = {
-  title: body('title').exists().withMessage('must have a title').trim().escape(),
-  id: body('id').exists().withMessage('must have a valid id')
-}
 
 listRouter.route('/')
   .get(actions.getAll)
